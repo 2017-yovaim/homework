2a1a502eef06b9382fd1f7ebbf13d05c80660384
kids <kids@petia>
2020-10-27 08:46:12 +0200

    Sixth Commit(tried to add day of the week determination algorithm)

--- src/edu/school/problems/Date.java
+++ src/edu/school/problems/Date.java
@@ -122,6 +122,7 @@ public class Date {
 	
 	public String toString() {
 		String result = hour + ":" + minutes + ":" + seconds + ", " + day + "." + month + "." + year;
+		result += "\nDay of the week: " + getDayOfTheWeek();
 		return(result);
 	}
 
@@ -232,6 +233,17 @@ public class Date {
 			month = 1;
 			year++;
 		}
+	}
+	
+	public DayOfWeek getDayOfTheWeek() {
+		Date temp = this;
+		int[] t = {0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4};
+		if(temp.month < 3) {
+			temp.year--;
+		}
+		int result1 = (temp.year + temp.year / 4 - temp.year / 100 + temp.year / 400 + t[temp.month - 1] + temp.day) % 7;
+		DayOfWeek result = getDayOfWeek(result1);
+		return(result);
 	}
 
 }
--- src/edu/school/problems/DayOfWeek.java
+++ src/edu/school/problems/DayOfWeek.java
@@ -1,7 +1,7 @@
 package edu.school.problems;
 
 public enum DayOfWeek {
-	MONDAY(1), TUESDAY(2), WEDNESDAY(3), THURSDAY(4), FRIDAY(5), SATURDAY(6), SUNDAY(7);
+	MONDAY(1), TUESDAY(2), WEDNESDAY(3), THURSDAY(4), FRIDAY(5), SATURDAY(6), SUNDAY(0);
 
 	private int weekDay;
 
@@ -73,5 +73,21 @@ public enum DayOfWeek {
 		return(result);
 	}
 	
-	//public static int 
+	public static DayOfWeek getDayOfWeek(int v) {
+		if(v == 1)
+			return MONDAY;
+		else if(v == 2)
+			return TUESDAY;
+		else if(v == 3)
+			return WEDNESDAY;
+		else if(v == 4)
+			return THURSDAY;
+		else if(v == 5)
+			return FRIDAY;
+		else if(v == 6)
+			return SATURDAY;
+		else if (v == 0) 
+			return SUNDAY;
+		return null;
+	}
 }
