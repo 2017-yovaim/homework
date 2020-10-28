08d2de9396f227f2403bf47371a5b290a5967ca0
kids <kids@petia>
2020-10-28 09:22:25 +0200

    Seventh Commit(fixed toString(int) using toString())

--- src/edu/school/problems/DayOfWeek.java
+++ src/edu/school/problems/DayOfWeek.java
@@ -27,47 +27,9 @@ public enum DayOfWeek {
 
 	public String toString(int numberOfLetters) {
 		String result = "";
-		String day = "";
-		if (weekDay == 1) {
-			day = "Monday";
-			for (int i = 0; i < numberOfLetters; i++) {
-				result += day.charAt(i);
-			}
-		} else if (weekDay == 2) {
-			day = "Tuesday";
-			for (int i = 0; i < numberOfLetters; i++) {
-				result += day.charAt(i);
-			}
-		}
-		else if(weekDay == 3) {
-			day = "Wednesday";
-			for (int i = 0; i < numberOfLetters; i++) {
-				result += day.charAt(i);
-			}
-		}
-		else if(weekDay == 4) {
-			day = "Thursday";
-			for (int i = 0; i < numberOfLetters; i++) {
-				result += day.charAt(i);
-			}
-		}
-		else if(weekDay == 5) {
-			day = "Friday";
-			for (int i = 0; i < numberOfLetters; i++) {
-				result += day.charAt(i);
-			}
-		}
-		else if(weekDay == 6) {
-			day = "Saturday";
-			for (int i = 0; i < numberOfLetters; i++) {
-				result += day.charAt(i);
-			}
-		}
-		else {
-			day = "Sunday";
-			for (int i = 0; i < numberOfLetters; i++) {
-				result += day.charAt(i);
-			}
+		String day = this.toString();
+		for(int i = 0; i < numberOfLetters; i++) {
+			result += day.charAt(i);
 		}
 		
 		return(result);
